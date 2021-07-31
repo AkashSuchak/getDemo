07d97a38f8c7e515eed5635b1ca539ebd43014cd
AkashSuchak <72850833+AkashSuchak@users.noreply.github.com>
2021-07-31 21:17:56 +0530

    feat: refactor: message

--- getDemo/Program.cs
+++ getDemo/Program.cs
@@ -6,7 +6,7 @@ namespace getDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Akash!");
         }
     }
 }
